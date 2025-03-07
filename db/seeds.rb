ab24913607fd6ea3b10761988212838a28c68915
elitwill <evan_litwiller@taylor.edu>
2025-03-07 09:53:18 -0500

    Depot Scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.delete_all
+#...
+Product.create!(title: 'Design and Build Great Web APIs',
+    description: 
+        %{<p>
+          <em>Robust, Reliable, and Resilient</em>
+          APIs are transforming the business world at an increasing pace. Gain
+          the essential skills needed to quickly desing, build, and deploy
+          quality APIs that are robust, reliable, and resilient. Go from 
+          initial design through prototyping and implementation to deployment of 
+          mission-critical APIs for your organization. Test, secure, and deploy
+          you API with confidence and aviod the "release into production" 
+          panic. Tackle just about any API cahllenge with more than a dozen 
+          open-source utilities and common programming patterns you can apply 
+          right away.
+        </p>}
+    image_url: 'maapis.jpg',
+    price: 24.95)
+#...
