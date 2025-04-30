435825b59dd4aaa275e49c6a20249acbf8c27df1
elitwill <evan_litwiller@taylor.edu>
2025-04-30 10:19:01 -0400

    added products in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,31 @@ Product.create!(title: 'Design and Build Great Web APIs',
     image_url: 'maapis.jpg',
     price: 24.95)
 #...
+Product.create!(title: 'Docker for Rails Developers',
+    description: 
+        %{<p>
+          <em>Build, Ship, and Run Your Applications Everywhere </em> Docker 
+          does for DevOps what Rails did for web development-it gives you a 
+          new set of superpowers. Gone are "works on my machine" woes and 
+          lengthy setup tasks, replaced instead by a simple, consistent, 
+          Docker-based development environment that will have your team up 
+          and running in seconds.
+        </p>},
+    image_url: 'ridocker.jpg',
+    price: 19.95)
+#...
+Product.create!(title: 'Modern CSS with Tailwind',
+    description: 
+        %{<p>
+          <em>Flexible Styling Without the Fuss</em> Tailwind CSS is an 
+          exciting new CSS framework that allows you to design your site by 
+          composing simple utility classes to create complex effects. With 
+          Tailwind, you can style your text, move your items on the page, 
+          design complex page layouts, and adapt your design for devices from 
+          a phone to a wide-screen monitor. With this book, you'll learn how 
+          to use the Tailwind for its flexibility and its consistency, from the 
+          smallest detail of your typography to the entire design of your site.
+        </p>},
+    image_url: 'tailwind.jpg',
+    price: 18.95)
+#...
