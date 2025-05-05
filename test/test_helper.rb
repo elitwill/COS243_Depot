473e900aab411f7d498a30fc5f331080ccdc022e
elitwill <evan_litwiller@taylor.edu>
2025-05-05 09:00:33 -0400

    finished ch 12 - tests still not working but unable to fix with Dr. Read

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,3 +11,30 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+# TRIED THIS
+# ----------
+# ENV["RAILS_ENV"] ||= "test"
+# require_relative "../config/environment"
+# require "rails/test_help"
+
+# require "capybara/rails"
+# require "capybara/minitest"
+
+# Capybara.register_driver :custom_chrome_headless do |app|
+#   options = Selenium::WebDriver::Chrome::Options.new
+#   options.add_argument("--headless")
+#   options.add_argument("--disable-gpu")
+#   options.add_argument("--window-size=1400,1400")
+
+#   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+# end
+
+# class ActiveSupport::TestCase
+#   # Run tests in parallel with specified workers
+#   parallelize(workers: :number_of_processors)
+
+#   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+#   fixtures :all
+# end
+
