ed2f05143e35baa0066ea92cba996a70089bf75d
elitwill <evan_litwiller@taylor.edu>
2025-05-12 18:25:26 -0400

    nearing the end of not going to try much more

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Depot
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+      # Add these lines:
+      config.i18n.available_locales = [:en, :es]
+      config.i18n.default_locale = :en
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
